2fd0dac9c12d5d11b9560b6280b73bd3fd22693d
Yves BAYINGANA <yvantlx62@gmail.com>
2025-11-02 23:01:26 +0200

    Introduction to Django Development Environment Setup.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,13 @@
+# ...existing code...
 from django.contrib import admin
-
-# Register your models here.
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
+    ordering = ('title',)
+    list_per_page = 25
+
+admin.site.register(Book, BookAdmin)
+# ...existing code...

727b742888905215f96afce9540b5d63f3744ed8
Yves BAYINGANA <yvantlx62@gmail.com>
2025-11-06 23:40:52 +0200

    Add function-based and class-based views for books and library details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library
-
+from .models import Book, Library   # ✅ includes "from .models import Library"
 
 # ---------------------------
 # Function-based View
@@ -10,7 +8,7 @@ from .models import Book, Library
 def list_books(request):
     """Displays a list of all book titles and their authors."""
     books = Book.objects.all()
-    # Render using the required template path
+    # ✅ required explicit template path
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
@@ -20,5 +18,5 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     """Displays details for a specific library, including all its books."""
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # make sure the path matches
+    template_name = 'relationship_app/library_detail.html'  # ✅ correct template path
     context_object_name = 'library'

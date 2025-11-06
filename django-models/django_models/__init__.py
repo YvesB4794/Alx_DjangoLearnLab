41e46f95a2e1f597c8ba8dad20b7ea083de02e7a
Yves BAYINGANA <yvantlx62@gmail.com>
2025-11-06 14:18:30 +0200

    Task:  add relationship_app with complex models and query_samples script (django-models)

--- django-models/django_models/__init__.py
+++
@@ -1,3 +0,0 @@
-# The contents of the file /django-models/django-models/django_models/__init__.py
-
-# This file is intentionally left blank.
--- django-models/relationship_app/__init__.py
+++
@@ -1,3 +0,0 @@
-# The contents of the file /django-models/relationship_app/__init__.py
-
-# This file is intentionally left blank.

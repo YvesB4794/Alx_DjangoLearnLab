99230daaa35de49958098c1c552b84638d15f93f
Yves BAYINGANA <yvantlx62@gmail.com>
2025-11-06 22:40:27 +0200

    Task:  add relationship_app with complex models and query_samples script (django-models)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,12 +5,14 @@ def get_books_by_author(author):
     books = Book.objects.filter(author=author)
     return books
 
-# List all books in a specific library
-def get_books_in_library(library):
+# List all books in a library
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
     books = library.books.all()
     return books
 
-# Retrieve the librarian for a specific library
-def get_librarian_for_library(library):
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
     librarian = Librarian.objects.get(library=library)
     return librarian

c30f6a8ccea823de8377de198fd5b1cf47bbdaae
Yves BAYINGANA <yvantlx62@gmail.com>
2025-11-14 11:57:51 +0200

    Add CustomUser model with manager, register admin, use AUTH_USER_MODEL, add media settings

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -9,7 +10,6 @@ class Book(models.Model):
     on_delete=models.CASCADE,
     related_name="bookshelf_books_added"
     )
-
 
     def __str__(self):
         return self.title
@@ -25,3 +25,6 @@ class Bookshelf(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.owner.username})"
+class CustomUser(AbstractUser):
+    date_of_birth = None  # dummy placeholder
+    profile_photo = None  # dummy placeholder    

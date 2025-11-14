56cff219a96595e50095c0c110acfe365c729f8b
Yves BAYINGANA <yvantlx62@gmail.com>
2025-11-14 10:36:16 +0200

    Add CustomUser model with manager, register admin, use AUTH_USER_MODEL, add media settings

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
     bio = models.TextField(blank=True, null=True)
 

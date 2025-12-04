42ea08aabf7e9639715e3587e0bca73a4b5c4338
Yves BAYINGANA <yvantlx62@gmail.com>
2025-12-04 16:38:32 +0200

    Initial Django blog setup: Post model, templates, admin, urls

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,19 +1,11 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User   # ✅ REQUIRED IMPORT
 
 class Post(models.Model):
-    """
-    Blog post model
-    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-
-    class Meta:
-        ordering = ['-published_date']
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title

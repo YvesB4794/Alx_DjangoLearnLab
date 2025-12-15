3df9fdfa76530b9aad2f62e37d0835e481a11451
Yves BAYINGANA <yvantlx62@gmail.com>
2025-12-15 14:44:30 +0200

    Add Heroku deployment files to repo root

--- social_media_api/social_media_api/wsgi.py
+++ social_media_api/social_media_api/wsgi.py
@@ -7,12 +7,28 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+#mport os
+
+#rom django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
+
+#application = get_wsgi_application()
+
+
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+def main():
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE',
+        'social_media_api.settings'
+    )
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise
+    execute_from_command_line(sys.argv)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
-
-application = get_wsgi_application()
-
-
+if __name__ == '__main__':
+    main()
